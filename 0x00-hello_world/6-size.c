b79be158430d045649c91aadd4b2a3bb81aececf
Towett-Hass <towetthass@gmail.com>
2023-06-16 21:32:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,12 @@
-6-size.c
+#include <stdio.h>
+/**
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
