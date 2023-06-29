2d5cbc5246d896a183b7f767efbec2c29daf2e57
Towett-Hass <towetthass@gmail.com>
2023-06-29 15:57:02 -0700

    0-strct

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,29 +2,18 @@
 
 /**
  *  * _strcat - concatenates two strings
- *   * @dest: string to append to
- *    * @src: string to add
- *     *
- *      * Return: a pointer to the resulting string
- *       */
+ *   * @dest: destination string
+ *    * @src: source string
+ *     * Return: pointer to resulting string dest
+ *      */
 char *_strcat(char *dest, char *src)
 {
 		int i, j;
 
-			i = 0;
-				j = 0;
-
-					while (dest[i] != '\0')
-								i++;
-
-						while (src[j] != '\0')
-								{
-											dest[i] = src[j];
-													j++;
-															i++;
-																}
-
-							dest[i] = '\0';
-
-								return (dest);
+			for (i = 0; dest[i]; i++)
+						;
+				for (j = 0; src[j]; j++)
+							dest[i + j] = src[j];
+					dest[i + j] = '\0';
+						return (dest);
 }
